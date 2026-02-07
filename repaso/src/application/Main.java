110527bbca6935dd0b401230d673818ad571c266
Ssalazar62 <santiago.sk08@gmail.com>
2026-02-07 14:10:32 -0500

    repaso java

---
+++ repaso/src/application/Main.java
@@ -0,0 +1,23 @@
+package application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+   String nombre = "isaac garcia yepes";
+   int edad = 17;
+   boolean male = true;
+   System.out.println("hi my name is " + nombre);
+        System.out.println("my age is "+edad);
+        System.out.println("im male? "+male);
+    int AÑOACTUAL = 2026;
+    int año_Nacimiento = 2009;
+    int calculateAge = AÑOACTUAL - año_Nacimiento;
+
+        System.out.println("mi edad se calcula "+calculateAge);
+
+for (int i=1;i<11;i++)
+{System.out.println(i);}
+
+    }
+}
