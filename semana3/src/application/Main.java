66581adc4ae4c940345e2d28f31df9bf232061c0
Ssalazar62 <santiago.sk08@gmail.com>
2026-02-14 12:11:15 -0500

    didefinicion de variablas y metodos arreglado

--- semana3/src/application/Main.java
+++ semana3/src/application/Main.java
@@ -1,2 +1,36 @@
-package application;public class Main {
+package application;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    final static int CURRENT_YEAR = 2026;
+
+
+    public static void main(String[] args) {
+
+        final String GREETING_MESSAGE = "saludo estufdiantes de java";
+        String[] nameStudents = {"juan", "maria", "pedro", "ana", "isa"};
+        int[] birthYears = {2000, 2020, 2008, 2009, 2001};
+        List<int[]> ageStudentList = new ArrayList<>();
+showMessage(GREETING_MESSAGE);
+
+
+
+        }
+
+
+
+
+
+    static void showMessage(String message){
+        System.out.println(message);
+    }
+
+    static int calculate(int bithYear) {
+        return CURRENT_YEAR - bithYear;
+
+    }
+
+
+
 }
